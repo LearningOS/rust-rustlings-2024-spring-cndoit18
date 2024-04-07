4a4bdf0de485b78645acc91f33ec9dd5e256a94b
cndoit18 <cndoit18@outlook.com>
2024-04-07 18:14:08 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    match apples {
+        n @ 0..=40 => n * 2,
+        n @ _ => n,
+    }
+}
 
 // Don't modify this function!
 #[test]
